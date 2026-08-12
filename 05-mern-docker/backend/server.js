b6b322be9400c615e59ac37c5d5284f8511f2cb8
maulanaa121 <fm886211@gmail.com>
2026-08-12 16:13:23 +0700

    first docker exercise, using mern stack

---
+++ 05-mern-docker/backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Backend MERN berjalan!"
+    });
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server berjalan di port ${PORT}`);
+});
---
+++ 05-mern-docker/frontend/src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-950 flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold text-white">
+          MERN Docker
+        </h1>
+
+        <p className="mt-4 text-slate-400">
+          React + Tailwind CSS berhasil berjalan
+        </p>
+
+        <button className="mt-6 rounded-lg bg-blue-600 px-6 py-3 font-semibold text-white hover:bg-blue-700">
+          Mulai Project
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
