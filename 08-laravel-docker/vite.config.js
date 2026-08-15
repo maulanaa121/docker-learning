f03a8b924e623b25d9682e12686303edb1ffc557
Fajar Maulana <fm886211@gmail.com>
2026-08-15 12:10:08 +0700

    Add Tailwind CSS and Vite Docker setup

--- 08-laravel-docker/vite.config.js
+++ 08-laravel-docker/vite.config.js
@@ -16,8 +16,19 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+
     server: {
+        host: '0.0.0.0',
+        port: 5173,
+
+        hmr: {
+            host: 'localhost',
+            port: 5173,
+        },
+
         watch: {
+            usePolling: true,
+            interval: 100,
             ignored: ['**/storage/framework/views/**'],
         },
     },
