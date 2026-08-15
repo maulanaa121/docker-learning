578a628f4792cd129ec51fc320bfd6c6663750f6
Fajar Maulana <fm886211@gmail.com>
2026-08-15 13:00:36 +0700

    Fixing the Git configuration in WSL

--- 08-laravel-docker/resources/views/welcome.blade.php
+++ 08-laravel-docker/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Fajar Maulana — Portfolio</title>
+    <title>Fajar Maulana — Portfoli</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
